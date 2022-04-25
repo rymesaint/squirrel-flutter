2d3606d9afee1159f4b74ef8e2716dea4c03be69
RustamG <ryder.norco@gmail.com>
2022-04-25 12:43:14 +0400

    Ability to specify parameters via command line arguments. Now .exe name can be different from package name.

--- README.md
+++ README.md
@@ -1,14 +1,63 @@
 # squirrel
 
-A new Flutter package project.
+Squirrel for Flutter apps.
 
 ## Getting Started
 
-This project is a starting point for a Dart
-[package](https://flutter.dev/developing-packages/),
-a library module containing code that can be shared easily across
-multiple Flutter or Dart projects.
+### Installation
+```
+flutter pub add squirrel --dev --git-url https://github.com/RustamG/squirrel-flutter.git
+```
 
-For help getting started with Flutter, view our 
-[online documentation](https://flutter.dev/docs), which offers tutorials, 
-samples, guidance on mobile development, and a full API reference.
+### Launch
+
+```
+flutter build windows
+flutter pub run squirrel:installer_windows
+```
+
+
+### Configuration
+Parameters can be specified either in pubspec.yaml or passed as command-line arguments. Values passed as command-line arguments have higher priority.
+
+Example of specifying the parameters via command-line:
+```
+flutter pub run squirrel:installer_windows --authors "My comapny" --appFriendlyName "My App"
+```
+
+List of available parameters:
+- `releaseDirectory` - where the result files will be placed.
+- `packageName` (optional) - Name of the directory where your app will be installed. If not provided, the `name` from pubspec.yaml is used.
+- `mainExeName` (optional) - Name of application .exe file. Falls back to `packageName`.
+- `appFriendlyName` (required) - Name of the app. If not provided, `title` from pubspec.yaml is used.
+- `version` (optional) - Version of the package. If not provided, top-level value from pubspec.yaml is used.
+- `authors` (required) - Authors separated by comma. Required by NuGet. Falls back to top-level `authors` in pubspec.yaml which is currently [deprecated](https://dart.dev/tools/pub/pubspec#authorauthors).
+- `appDescription` (optional) - App description. Falls back to `appFriendlyName`.
+- `appIcon` (required) - Icon for the installer
+- `loadingGif` (optional) - A path to GIF file which will be displayed during initial installation of the app.
+- `uninstallIconPngUrl` (required) - URL of the icon for the uninstaller in PNG format, 128x128.
+- `setupIcon` (optional) - Path to a .ico file for the Setup.exe. Falls back to `appIcon`.
+- `releaseUrl` (optional) - URL to run SyncReleases.exe
+- `buildEnterpriseMsiPackage` (optional, default: false) - whether to build .msi. `--no-msi` flag for squirrel.exe
+- `dontBuildDeltas` (optional) - `--no-delta` flag for squirrel.exe
+- `certificateFile` - **TBD**
+- `overrideSigningParameters` - **TBD**
+
+
+Example of configuration via pubspec.yaml:
+```
+squirrel:
+  windows:
+    appIcon: "windows/runner/resources/app_icon.ico"
+    appFriendlyName: "blaf"
+    appDescription: "blaf"
+    authors: "Bob"
+    certificateFile: "foo"
+    overrideSigningParameters: "bar"
+    loadingGif: "baz"
+    uninstallIconPngUrl: "blahhgh"
+    setupIcon: "bamf"
+    releaseDirectory: "blaf"
+    buildEnterpriseMsiPackage: false
+    dontBuildDeltas: false
+```
--- bin/installer_windows.dart
+++ bin/installer_windows.dart
@@ -5,222 +5,22 @@ library installer_windows;
 import 'dart:io';
 
 import 'package:jinja/jinja.dart';
-import 'package:package_config/package_config.dart' show findPackageConfig;
 import 'package:path/path.dart' as path;
+import 'package:squirrel/parameters.dart';
+import 'package:squirrel/utils.dart';
 import 'package:yaml/yaml.dart';
 
-late String rootDir;
-late String appDir;
-
-Future<void> _initPaths() async {
-  final packagesConfig = await findPackageConfig(Directory.current);
-  if (packagesConfig == null) {
-    throw 'Failed to locate or read package config.';
-  }
-
-  final squirrelPackage = packagesConfig.packages
-      .firstWhere((package) => package.name == 'squirrel');
-  rootDir = path.join(path.fromUri(squirrelPackage.packageUriRoot), '..');
-  appDir = path.canonicalize(
-      path.join(path.dirname(path.fromUri(Platform.packageConfig!)), '..'));
-}
-
-final pubspecYaml = path.join(appDir, 'pubspec.yaml');
-
-const youDontHaveAProperPubspec = '''
-Your app's pubspec.yaml doesn't have a Squirrel section, it is Highly Recommended 
-to have one! Here's an example section with all of the available parameters, though
-most are optional:
-
-squirrel:
-  windows:
-    certificateFile: "foo"
-    overrideSigningParameters: "bar"
-    loadingGif: "baz"
-    appIcon: "blamf"
-    uninstallIconPngUrl: "blahhgh"
-    appFriendlyName: "blaf"
-    appDescription: "blaf"
-    setupIcon: "bamf"
-    releaseDirectory: "blaf"
-    buildEnterpriseMsiPackage: false
-    dontBuildDeltas: false
-''';
-
-String stringOrThrow(dynamic? d, String err) {
-  if (d == null) {
-    throw Exception(err);
-  }
-
-  return d.toString();
-}
-
-String parseVersion(dynamic? v) {
-  final ver = stringOrThrow(v, 'Your app needs a version');
-  return ver.replaceFirst(RegExp(r'[-+].*$'), '').trimLeft();
-}
-
-String parseAuthor(dynamic? a) {
-  final author = stringOrThrow(a, 'Your pubspec needs an authors section');
-  return author.replaceAll(RegExp(r' <.*?>'), '').trimLeft();
-}
-
-String? canonicalizePubspecPath(String? relativePath) {
-  if (relativePath == null) {
-    return null;
-  }
-
-  if (path.isAbsolute(relativePath)) {
-    return relativePath;
-  }
-
-  if (relativePath.startsWith('http://') ||
-      relativePath.startsWith('https://')) {
-    return relativePath;
-  }
-
-  return path.normalize(path.join(appDir, relativePath));
-}
-
-String? generateSigningParams(String? certificateFile) {
-  final certPass = Platform.environment['SQUIRREL_CERT_PASSWORD'];
-  final overrideParams =
-      Platform.environment['SQUIRREL_OVERRIDE_SIGNING_PARAMS'];
-
-  if (overrideParams != null) {
-    return overrideParams;
-  }
-
-  if (certPass == null) {
-    if (certificateFile == null) return null;
-
-    throw Exception(
-        'You must set either the SQUIRREL_CERT_PASSWORD or the SQUIRREL_OVERRIDE_SIGNING_PARAMS environment variable');
-  }
-
-  return '/a /f \"$certificateFile\" /p $certPass /v /fd sha256 /tr http://timestamp.digicert.com /td sha256';
-}
-
-const defaultUninstallPngUrl = 'https://fill/in/this/later';
-
-class PubspecParams {
-  final String name;
-  final String title;
-  final String version;
-  final String authors;
-  final String description;
-  final String appIcon;
-  final String? certificateFile;
-  final String? overrideSigningParameters;
-  final String loadingGif;
-  final String? uninstallIconPngUrl;
-  final String? setupIcon;
-  final String releaseDirectory;
-  final String? releaseUrl;
-  final bool buildEnterpriseMsiPackage;
-  final bool dontBuildDeltas;
-
-  PubspecParams(
-      this.name,
-      this.title,
-      this.version,
-      this.authors,
-      this.description,
-      this.appIcon,
-      this.certificateFile,
-      this.overrideSigningParameters,
-      this.loadingGif,
-      this.uninstallIconPngUrl,
-      this.setupIcon,
-      this.releaseDirectory,
-      this.releaseUrl,
-      this.buildEnterpriseMsiPackage,
-      this.dontBuildDeltas);
-
-  factory PubspecParams.fromYaml(dynamic appPubspec) {
-    dynamic windowsSection = appPubspec['squirrel']['windows'];
-    if (windowsSection == null) {
-      stderr.writeln(youDontHaveAProperPubspec);
-      windowsSection = {};
-    }
-
-    final name = appPubspec['name'].toString();
-    final title = stringOrThrow(
-        windowsSection['appFriendlyName'] ?? appPubspec['title'],
-        'Your app needs a description!');
-    final version = parseVersion(appPubspec['version']);
-    final authors = parseAuthor(appPubspec['authors']);
-    final description = stringOrThrow(windowsSection['appDescription'] ?? title,
-        'Your app must have a description');
-    final appIcon = canonicalizePubspecPath(stringOrThrow(
-        windowsSection['appIcon'], 'Your app must have an icon'))!;
-    final certificateFile =
-        canonicalizePubspecPath(windowsSection['certificateFile']?.toString());
-    final overrideSigningParameters =
-        windowsSection['overrideSigningParameters']?.toString() ??
-            generateSigningParams(certificateFile);
-    final loadingGif = canonicalizePubspecPath((windowsSection['loadingGif'] ??
-            path.join(rootDir, 'vendor', 'default-loading.gif'))
-        .toString())!;
-    final uninstallIconPngUrl =
-        (windowsSection['uninstallIconPngUrl'] ?? defaultUninstallPngUrl)
-            .toString();
-    final setupIcon = canonicalizePubspecPath(
-        (windowsSection['setupIcon'] ?? appIcon).toString());
-    final releaseDirectory = canonicalizePubspecPath(
-        windowsSection['releaseDirectory']?.toString() ??
-            path.join(appDir, 'build'))!;
-    final releaseUrl = canonicalizePubspecPath((windowsSection['releaseUrl'] ??
-            Platform.environment['SQUIRREL_RELEASE_URL'])
-        ?.toString());
-    final buildEnterpriseMsiPackage =
-        windowsSection['buildEnterpriseMsiPackage'] == true ? true : false;
-    final dontBuildDeltas =
-        windowsSection['dontBuildDeltas'] == true ? true : false;
-
-    if (certificateFile != null && overrideSigningParameters != null) {}
-
-    return PubspecParams(
-        name,
-        title,
-        version,
-        authors,
-        description,
-        appIcon,
-        certificateFile,
-        overrideSigningParameters,
-        loadingGif,
-        uninstallIconPngUrl,
-        setupIcon,
-        releaseDirectory,
-        releaseUrl,
-        buildEnterpriseMsiPackage,
-        dontBuildDeltas);
-  }
-}
-
-Future<ProcessResult> runUtil(String name, List<String> args,
-    {String? cwd}) async {
-  final cmd = path.join(rootDir, 'vendor', name);
-  final ret = await Process.run(cmd, args, workingDirectory: cwd);
-
-  if (ret.exitCode != 0) {
-    final msg =
-        "Failed to run $cmd ${args.join(' ')}\n${ret.stdout}\n${ret.stderr}";
-    throw Exception(msg);
-  }
-
-  return ret;
-}
 
 Future<int> main(List<String> args) async {
-  await _initPaths();
+  await initPaths();
+
+  final pubspecYaml = path.join(appDir, 'pubspec.yaml');
   final yaml = loadYaml(await File(pubspecYaml).readAsString());
 
   final template = Environment().fromString(
       await File(path.join(rootDir, 'nuspec.jinja')).readAsString());
 
-  final pubspec = PubspecParams.fromYaml(yaml);
+  final pubspec = Parameters.fromYamlAndArgs(yaml, args);
   final buildDirectory = canonicalizePubspecPath(
       path.join('build', 'windows', 'runner', 'Release'))!;
 
@@ -239,7 +39,7 @@ Future<int> main(List<String> args) async {
   await runUtil('rcedit.exe', [
     '--set-icon',
     pubspec.appIcon,
-    path.join(buildDirectory, '${pubspec.name}.exe')
+    path.join(buildDirectory, '${pubspec.mainExeName}.exe')
   ]);
 
   // ls -r to get our file tree and create a temp dir
@@ -251,9 +51,9 @@ Future<int> main(List<String> args) async {
 
   final nuspecContent = template
       .render(
-          name: pubspec.name,
-          title: pubspec.title,
-          description: pubspec.description,
+          name: pubspec.packageName,
+          title: pubspec.appFriendlyName,
+          description: pubspec.appDescription,
           version: pubspec.version,
           authors: pubspec.authors,
           iconUrl: pubspec.uninstallIconPngUrl,
@@ -304,7 +104,7 @@ SQUIRREL_I_KNOW_THAT_USING_HTTP_URLS_IS_REALLY_BAD to 'I hereby promise.'
   }
 
   // Releasify!
-  var args = [
+  var squirrelExeArgs = [
     '--releasify',
     nupkgFile,
     '--releaseDir',
@@ -317,21 +117,21 @@ SQUIRREL_I_KNOW_THAT_USING_HTTP_URLS_IS_REALLY_BAD to 'I hereby promise.'
   // parameters so from the perspective of this part of the code, there is *only*
   // either a "custom" signing parameter, or nothing
   if (pubspec.overrideSigningParameters != null) {
-    args.addAll(['-n', pubspec.overrideSigningParameters!]);
+    squirrelExeArgs.addAll(['-n', pubspec.overrideSigningParameters!]);
   }
 
   if (pubspec.dontBuildDeltas) {
-    args.add('--no-delta');
+    squirrelExeArgs.add('--no-delta');
   }
 
   if (!pubspec.buildEnterpriseMsiPackage) {
-    args.add('--no-msi');
+    squirrelExeArgs.add('--no-msi');
   }
 
   if (pubspec.setupIcon != null) {
-    args.addAll(['--setupIcon', pubspec.setupIcon!]);
+    squirrelExeArgs.addAll(['--setupIcon', pubspec.setupIcon!]);
   }
 
-  await runUtil('squirrel.exe', args);
+  await runUtil('squirrel.exe', squirrelExeArgs);
   return 0;
 }
